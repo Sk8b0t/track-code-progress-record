85a5a3292def3dc1b820e83643477c13e5f5a28e
सायन <sayanbiswas0812@gmail.com>
2025-12-31 15:40:01 +0530

    dunder method learnt

---
+++ practice+learning/dundun.py
@@ -0,0 +1,25 @@
+class repairingShop():
+    def __init__(self,name,brand,no,amt):
+        self.name=name
+        self.brand=brand
+        self.phoneNumber=no
+        self.amount=amt
+    def printDetails(self):
+        print("Name of Customer:",self.name)
+        print("brand:",self.name)
+        print("contact no:",self.name)
+    def __add__(self, other):
+        return f"Total money earned = {self.amount+other.amount}"
+    def __floordiv__(self,other):
+        return self.amount//other.amount
+    def __pos__(self):
+        if self.amount>0:
+            return "positive earning"
+
+if __name__ == '__main__':
+    C1=repairingShop("Sayan Biswas","Acer",8969101484,3800)
+    C2=repairingShop("Likitha Reddy","Lenovo",7209537974,5000)
+    print(C1+C2)
+    print(C2//C1)
+    print(+C1)
+
