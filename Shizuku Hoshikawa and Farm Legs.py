c9aa98b64d148e9f05208f66f4a5b0ab400cbb3d
सायन <sayanbiswas0812@gmail.com>
2026-01-07 13:30:23 +0530

    problem 4 short handed

--- Shizuku Hoshikawa and Farm Legs.py
+++ Shizuku Hoshikawa and Farm Legs.py
@@ -1,29 +1,38 @@
+# t=int(input())
+# for i in range(t):
+#     n=int(input())
+#     cnt=0
+#     if n%4==0 :
+#         chickens=n//2
+#         cows=0
+#         if chickens!=1:
+#             while(chickens!=0):
+                
+#                 chickens-=2
+#                 cows+=1
+#                 cnt+=1
+#             cnt+=1
+        
+#     elif n%2==0:
+#         chickens=n//2
+#         cows=0
+#         if chickens!=1:
+#             while(chickens!=1):
+                
+#                 chickens-=2
+#                 cows+=1
+#                 cnt+=1
+#         cnt+=1
+        
+#     print(cnt)
+
+
 t=int(input())
 for i in range(t):
     n=int(input())
-    cnt=0
-    if n%4==0 :
-        chickens=n//2
-        cows=0
-        if chickens!=1:
-            while(chickens!=0):
-                
-                chickens-=2
-                cows+=1
-                cnt+=1
-            cnt+=1
-        
-    elif n%2==0:
-        chickens=n//2
-        cows=0
-        if chickens!=1:
-            while(chickens!=1):
-                
-                chickens-=2
-                cows+=1
-                cnt+=1
-        cnt+=1
-        
-        
+    if n%2==0:
+        print((n//4) +1)
+    else:
+        print(0)
     
-    print(cnt)
+
