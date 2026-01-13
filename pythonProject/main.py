1968f0d2a215b489871d5df2aad6c97b1434863a
सायन <sayanbiswas0812@gmail.com>
2026-01-13 22:22:11 +0530

    deleted all waste shit

--- pythonProject/main.py
+++
@@ -1,5 +0,0 @@
-a=int(input("enter a no."))
-b=int(input("enter a no."))
-if a<1:
- print("hello world")
-print("fck") if a>b else print("bye")
--- pythonProject6/main.py
+++
@@ -1,7 +0,0 @@
-import webbrowser
-
-url = 'https://www.youtube.com'
-
-# Open 100 tabs
-for i in range(100):
-    webbrowser.open_new_tab(url)
