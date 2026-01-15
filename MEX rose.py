b552b493a4adb94aa61cbdb999e6dc724a95f72d
सायन <sayanbiswas0812@gmail.com>
2026-01-15 17:10:25 +0530

    learnt something new today

--- MEX rose.py
+++ MEX rose.py
@@ -1,3 +1,4 @@
+#1
 t = int(input())
 for _ in range(t):
     n, k = map(int, input().split())
@@ -13,4 +14,46 @@ for _ in range(t):
                 cnt+=1
                 dup=item
     print(max(k-cnt,kcnt))
+#2 - using set
+# t = int(input())
+# for _ in range(t):
+#     n, k = map(int, input().split())
+#     a = list(map(int, input().split()))
+#     kcnt=0
+#     s=set()
+#     for item in a:
+#         if item==k:
+#             kcnt+=1
+#         if item<k:
+#             if item not in s:
+#                 set.add(item)
+                
+
+#     print(max(k-len(s),kcnt))
+
+#3 - using dictionary
+# t = int(input())
+# for _ in range(t):
+#     n, k = map(int, input().split())
+#     a = list(map(int, input().split()))
+#     cnt,kcnt=0=0
+#     freq{}
+#     for item in a:
+#         if item not in freq:
+#             freq[item]=1
+#         else:
+#             freq[item]+=1
+#     i=0
+#     while(i<k):
+#         if i not in freq:
+#             cnt+=1
+#         i+=1
+#     if k in freq:
+#         kcnt=freq[k]
+
+#     print(max(kcnt,cnt))
+            
+            
+        
+
 
