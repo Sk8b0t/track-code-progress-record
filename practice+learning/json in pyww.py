5204abdc57c92a700ef12f159aef51137ca3c921
सायन <sayanbiswas0812@gmail.com>
2026-01-30 00:32:56 +0530

    learned to handle some json shit in py

---
+++ practice+learning/json in pyww.py
@@ -0,0 +1,15 @@
+import json
+with open("learnJson.json","r") as f:
+    data=json.load(f)
+for det in data["emp details"]:
+    print(det)
+#json.load(takes string objects) and json.loads(takes file ovject)
+data2={
+     "sayan":"biswas",
+     "val":["vandel","phantom"],
+     "fridge":("roti",696),
+     "bool value":True
+ }
+print(json.dumps(data2,indent=4,sort_keys=True))
+#indent-> for spacing stuff
+#sort_keys-> for alphabetically sorting the keys
