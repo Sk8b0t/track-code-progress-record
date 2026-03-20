bc241c880272e9a8eb13bf84fb7dcd9b4cf12c79
सायन <sayanbiswas0812@gmail.com>
2026-03-20 11:33:09 +0530

    tkintering in python GUI

--- tkinter gui/attributes in tkinter.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-from PIL import Image,ImageTk
-root=Tk()
-root.geometry("900x900")
-root.title("By Sk8")
-
-root.mainloop()
---
+++ tkinter gui/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+f2=Frame(root, borderwidth=6, height=50,width=433)
+# f2.pack(side=TOP)
