9785a3ccf05cd55eb3a53ce225d125cbc65570d6
सायन <sayanbiswas0812@gmail.com>
2026-04-30 03:03:30 +0530

    custom packages bullshit

---
+++ my first python custom  package/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="kuchnhi_package",
+      version="0.3      ",
+      description="this is my first custom python package",
+      long_description="this is a very long description of my first custom python package",
+      author="sk8",
+      packages=['kuchnhi_package'],
+      install_requires=[])
