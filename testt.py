65b1591e27a587dcfab33da27d1c21bf688c92fb
सायन <sayanbiswas0812@gmail.com>
2026-05-10 13:48:14 +0530

    not so easy as I thought. Needed a little more brain~1000. The earlier one gave TLE

--- testt.py
+++ testt.py
@@ -1,15 +1,18 @@
-t=int(input())
+t = int(input())
+
 while t:
-    n,k=map(int,input().split())
-    a=list(map(int,input().split()))
-    newa=[]
-    while(len(newa)!=n):
-        ind=a.index(max(a))
-        a[ind]-=k
-        if a[ind]<=0:
-            newa.append(ind+1)
-    print(*newa)
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    ans=[]
+    for i in range(n):
+        r=a[i]%k
+        if r==0:
+            r=k
+        ans.append((-r,i+1))
+    ans.sort()
+    for _,index in ans:
+        print(index,end=" ")
+    print()
+        
 
-
-
-    t-=1
+    t -= 1
