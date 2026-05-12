a6feefbb69a2dffa57bc813c2adf9b236742a901
सायन <sayanbiswas0812@gmail.com>
2026-05-12 19:16:17 +0530

    solved another ~1000 rated problem

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-import math
-print(math.ceil(6/5))
+a=[1,1,3,1]
+fa={}
+ele=a[0]
+cnt=0
+a.append(-1)
+for i in range(len(a)):
+        if a[i]==ele:
+            cnt+=1
+        else:
+            if a[i-1] not in fa:
+                fa[a[i-1]]=0
+            fa[a[i-1]]=max(fa[a[i-1]],cnt)
+            ele=a[i]
+            cnt=1
+print(fa)
