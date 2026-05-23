be529efab3a3010a3fb404a43e78d08fae813cc1
Sk8 <sayanbiswas0812@gmail.com>
2026-05-24 04:00:34 +0530

    q3 @pandas done learnt about fillna dropna and isna etc.

--- pandas/testtt.py
+++ pandas/testtt.py
@@ -1,5 +1,11 @@
 import numpy as np
+import pandas as pd 
 
-print(np.random.rand(5,10)*10)
-print(np.arange(10))
-print(len(np.random.rand(50,10)))
+data = {
+    'Name': ['Aparna', 'Pankaj', 'Ram', 'Ramesh', 'Naveen', 'Krrishnav', 'Bhawna'],
+    'Degree': ['MBA', 'BCA', 'M.Tech', 'MBA', np.nan, 'BCA', 'MBA'],
+    'Score': [90.0, np.nan, 80.0, 98.0, 97.0, 78.0, 89.0]
+}
+df=pd.DataFrame(data)
+print(df)
+print(df.groupby("Degree")["Score"].max())
