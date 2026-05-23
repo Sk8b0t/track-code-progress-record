9ed496541dd3700b45d4114ebf759b0d5bc771fc
Sk8 <sayanbiswas0812@gmail.com>
2026-05-24 00:08:29 +0530

    q1 practice @pandas

--- pandas/q1.py
+++ pandas/q1.py
@@ -1,9 +1,22 @@
-import numpy as np
 import pandas as pd
-df=pd.DataFrame(np.random.rand(3,2)*10,index=[f"row{i}" for i in range(3)],columns=[f"column-{i}" for i in range(2)])
+df = pd.DataFrame({
+    "Name": ["A", "B", "C", "D", "E", "F"],
+    "Math": [78, 90, 67, 88, 95, 70],
+    "Physics": [80, 85, 70, 92, 96, 65],
+    "Chemistry": [75, 89, 72, 90, 98, 60]
+})
+df.loc[:,["Average"]]=(df.loc[:,"Math"]+df.loc[:,"Physics"]+df.loc[:,"Chemistry"])/3
 print(df)
-print(df.describe())
-print(df.mean())
-print(df.corr())
-print(df.std())
-print("min:",df.min())
+print("Topper marks:",df["Average"].max())
+print(df.sort_values(by="Average",ascending=False))
+abv=df.loc[(df['Math']>85) & (df["Physics"]>85) & (df["Chemistry"]>85)]
+print("students above 85 marks in all subjects are:\n")
+print(abv)
+# df["Result"]="Pass" if df["Average"]>=75 else "Fail"
+df.loc[:,"Result"]="Fail"
+df.loc[df["Average"]>=75,"Result"]="Pass"
+print(df)
+
+
+
+
