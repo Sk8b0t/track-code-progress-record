c71ab841c77748563075704e69c7c76f2df083fb
Sk8 <sayanbiswas0812@gmail.com>
2026-07-01 19:12:58 +0530

    learnt about functions
    learnt how to pass by value and pass by reference

---
+++ funcrions.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void inc(int &n1,int &n2){
+    n1++;
+    n2++;
+}
+int main(){
+    int a=4,b=5;
+    cout << a<<" "<<b<<endl;
+    inc(a,b);
+    cout << a<<" "<<b<<endl;
+
+}
