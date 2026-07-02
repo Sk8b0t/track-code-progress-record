757f4ef6a4fe2fabd1a58998d100cb3e741661aa
Sk8 <sayanbiswas0812@gmail.com>
2026-07-02 13:42:48 +0530

    learnt c++ concepts imp for cp

--- funcrions.cpp
+++ funcrions.cpp
@@ -4,10 +4,15 @@ void inc(int &n1,int &n2){
     n1++;
     n2++;
 }
+void swap(int &a,int &b){
+    int temp=a;
+    a=b;
+    b=temp;
+}
 int main(){
-    int a=4,b=5;
+    int a=89,b=5;
     cout << a<<" "<<b<<endl;
-    inc(a,b);
+    swap(a,b);
     cout << a<<" "<<b<<endl;
 
 }
