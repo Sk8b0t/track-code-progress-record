ee8158a6039b2a8a0bbff90ad1e01258e762dc56
Sk8 <sayanbiswas0812@gmail.com>
2026-07-08 14:18:33 +0530

    maps in c++

---
+++ maps.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    map<string,int> m;
+    for(int i=0;i<n;++i){
+        string s;
+        cin>>s;
+        m[s]++;
+    }
+    for(auto item: m) cout<<item.first<<" "<<item.second<<endl;
+}
