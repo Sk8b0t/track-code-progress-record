18ae66afe2e98a3f930746148cdd813e06b7817e
Sk8 <sayanbiswas0812@gmail.com>
2026-07-10 04:19:53 +0530

    hard ass qn on stack leetcode

--- LeetCode/Longest Valid Parentheses.cpp
+++ LeetCode/Longest Valid Parentheses.cpp
@@ -1,21 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 int longestValidParentheses(string s) {
-    stack<char> st;
-    unordered_map<char,int>m={{'[',-3},{'{',-2},{'(',-1},{']',3},{'}',2},{')',1}};
+    stack<int>st;
+    stack<char>chk;
+    unordered_map<char,int>m={{'(',-1},{')',1}};
     int cnt=0;
-    for(char c:s){
-        if(m[c]<0) st.push(c);
-        else{
-            if(!s.empty()|| m[st.top()]+m[c]==0) cnt+=1;
-        }
+    vector<int>ind(s.size(),1);
+
+   for(int i=0;i<s.size();++i){
+    if(m[s[i]]<0) {
+        st.push(i);
+        chk.push(s[i]);
     }
-    return cnt*2;
+    else{
+        if(!st.empty() && (m[chk.top()] + m[s[i]] == 0)){
+            ind[st.top()]=0;
+            ind[i]=0;
+            st.pop();
+            chk.pop();
+        }                    
+    }
+   }
+   int l=0,r=0;
+   int mx=0;
+   while(r<ind.size()){
+    if(ind[r]==0) mx=max(mx,r-l+1);
+    else l=r+1;
+    ++r;
+   }
+   return mx;
+   
 }
 int main(){
- string s = ")()())";
+ string s ="(()";
  cout<<longestValidParentheses(s);
-
-
 
 }
--- test.cpp
+++ test.cpp
@@ -1,40 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 int longestValidParentheses(string s) {
-    stack<int>st;
-    stack<char>chk;
     unordered_map<char,int>m={{'(',-1},{')',1}};
-    int cnt=0;
     vector<int>ind(s.size(),1);
+    stack<char>chk;
+    stack<int>st;
 
-   for(int i=0;i<s.size();++i){
-    if(m[s[i]]<0) {
-        st.push(i);
-        chk.push(s[i]);
+    for(int i=0;i<s.size();++i){
+        if(m[s[i]]<0){
+            chk.push(s[i]);
+            st.push(i);
+        }
+        else{
+            if(!st.empty()&&(m[st.top()]+m[i]==0)){
+                ind[st.top()]=0;
+                ind[i]=0;
+                st.pop();
+                chk.pop();
+            }
+        }
     }
-    else{
-        if(!st.empty() && (m[chk.top()] + m[s[i]] == 0)){
-            ind[st.top()]=0;
-            ind[i]=0;
-            st.pop();
-            chk.pop();
-        }                    
+    vector<int>ans;
+    int l=0,r=0,mx=0;
+    while(r<s.size()){
+        if(ind[r]==0) mx=max(mx,r-l+1);
+        else l=r+1;
+        ++r;
+
+        }
+        return mx;
     }
-   }
-   int l=0,r=0;
-   int mx=0;
-   while(r<ind.size()){
-    if(ind[r]==0){
-        mx=max(mx,r-l+1);
-    }
-    else l=r+1;
-    ++r;
-   }
-   return mx;
-   
-}
+
 int main(){
- string s ="(()";
- cout<<longestValidParentheses(s);
-
+    string s="(()))";
+    cout<<longestValidParentheses(s)<<endl;
 }
+
