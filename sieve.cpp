c10c2da744cccae6517b42b20edd4cb65fdf423f
Sk8 <sayanbiswas0812@gmail.com>
2026-07-24 03:10:41 +0530

    holy binary

--- sieve.cpp
+++ sieve.cpp
@@ -1,12 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const ll N=1e6+10;
-vector<bool>tprime(N,1);
+const int N=1e7+10;
+vector<bool>isPrime(N,1);
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    vector<bool>isPrime(N,1);
     isPrime[0]=isPrime[1]=false;
     for(int i=2;i<N;++i){
         if(isPrime[i]){
@@ -14,16 +13,10 @@ int main(){
                 isPrime[j]=false;
             }
         }
-    }
-    int n;
-    cin>>n;
-    vector<ll>x(n);
-    for(auto &itm:x) cin>>itm;
-    for(auto &itm:x){
-        ll sq=sqrt(itm);
-        if(sq*sq==itm && isPrime[sq]) cout<<"YES"<<"\n";
-        else cout<<"NO"<<"\n";
         
+    }
+    for(int i=1;i<30;++i){
+        cout<<isPrime[i]<<endl;
     }
 
     return 0;
