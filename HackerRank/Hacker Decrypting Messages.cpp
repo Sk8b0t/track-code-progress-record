0fedaccbafeadfd00566317e3ef484cd468e65bd
Sk8 <sayanbiswas0812@gmail.com>
2026-09-02 02:37:03 +0530

    Update Hacker Decrypting Messages.cpp

--- HackerRank/Hacker Decrypting Messages.cpp
+++ HackerRank/Hacker Decrypting Messages.cpp
@@ -1,64 +1,63 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const int N=1e6+4;
+const int N=1e6+7;
 int hp[N];
-bool canRemove[N];
 int hsh[N];
+bool chkHigherPower[N];
 
 vector<int>distinctPF(int x){
     vector<int>ans;
     while(x>1){
         int pf=hp[x];
-        while(x%pf==0) x/=pf;
+        while(x%pf==0)x/=pf;
         ans.push_back(pf);
     }
     return ans;
 }
 
-void solve(){
 
+void solve(){
     for(int i=2;i<N;++i){
         if(hp[i]==0){
             for(int j=i;j<N;j+=i) hp[j]=i;
         }
     }
-
+    
     int n,q;
     cin>>n>>q;
-    vector<ll>a(n);
+    vector<int>a(n);
     for(auto &itm:a){
         cin>>itm;
         hsh[itm]=1;
     }
     for(int i=2;i<N;++i){
         if(hsh[i]==1){
-            for(ll j=i;j<N;j*=i) canRemove[j]=true;
+            for(int j=i;j<N;j*=i) chkHigherPower[j]=true;
+
+            }
         }
-    }
     while(q--){
         int x;
         cin>>x;
         vector<int>pf=distinctPF(x);
-        bool isPossible=false;
+        bool hogya=false;
         for(int i=0;i<pf.size();++i){
             for(int j=i;j<pf.size();++j){
                 int pro=pf[i]*pf[j];
                 if(i==j && x%pro!=0) continue;
-                int toRemove=x/pro;
-                if(canRemove[toRemove]|| toRemove==1){
-                    isPossible=true;
+                int remove=x/pro;
+                if(chkHigherPower[remove]==1 || remove==1){
+                    hogya=true;
                     break;
                 }
             }
-            if(isPossible) break;
+            if(hogya) break;
         }
-        cout<<(isPossible?"YES\n":"NO\n");
+        cout<<(hogya?"YES\n":"NO\n");
 
     }
-
 }
-
 
 int main(){
     ios_base::sync_with_stdio(false);
